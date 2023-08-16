b3120caae07024fb0a54865fdd599a90d4b9f264
Vyshnav K S <vyshnav.ks@invenics.com>
2023-08-16 14:25:07 +0530

    UI of signup and login

--- fronend/src/pages/Login.js
+++ fronend/src/pages/Login.js
@@ -1,8 +1,41 @@
 import React from 'react'
+import './styles/login.css'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className="login-root">
+      <div className="login-container">
+        <div className="login-title">
+          Sign up to
+          <span className="login-logo">
+            Tech<span className="login-logo-blue">Hunt</span>
+          </span>
+        </div>
+        <div className="login-inputs">
+          <form className="login-form">
+            <input
+              className="login-input"
+              type="email"
+              size="15"
+              placeholder="Email ID"
+            ></input>
+            <input
+              className="login-input"
+              type="password"
+              size="15"
+              placeholder="Password"
+            ></input>
+           
+            <input className="login-submit" type="submit" value="Sign in" />
+          </form>
+          
+        </div>
+        <div className="signup-login-text">
+          Don't have an account? <Link to="/register" >Signup</Link>
+          </div>
+      </div>
+    </div>
   )
 }
 
