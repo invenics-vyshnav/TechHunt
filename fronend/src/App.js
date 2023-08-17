2a222e0bbef5adcb51406d7628f25c558d97a4d3
Invenics-DeekshaDevadiga <Deeksha.devadiga@invenics.com>
2023-08-17 12:44:48 +0530

    profile on header

--- fronend/src/App.js
+++ fronend/src/App.js
@@ -4,16 +4,22 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Jobs from './pages/Jobs';
+import Resource from './pages/Resource';
+
 
 function App() {
   return (
     <div >
     <BrowserRouter>
     <Routes>
-      <Route path="/home" element={<Home />}></Route>
-      <Route path="/register" element={<Signup />}></Route>
-      <Route exact path="/login" element={<Login />}></Route>
-      <Route exact path="/jobs" element={<Jobs />}></Route>
+      <Route index element={<Home/>}/>
+       <Route path="/home" element={<Home />}></Route> 
+     <Route path="/register" element={<Signup />}></Route>
+     <Route exact path="/login" element={<Login />}></Route>
+       <Route exact path="/jobs" element={<Jobs />}></Route>   
+       <Route path="/resource" element={<Resource />} /> 
+       <Route path="/resource" element={<Resource />} /> 
+   
     </Routes>
   </BrowserRouter>
     </div>
--- fronend/src/pages/Signup.js
+++ fronend/src/pages/Signup.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import "./styles/signup.css";
 import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
 import { apiSignup } from "../api/api";
 
 const Signup = () => {
